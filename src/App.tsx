a433ab401fb09b75d852dbe3114b96ca93635cab
Jalal Hosseini <jalalhosseiny@gmail.com>
2022-04-04 20:59:22 +0100

    add drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import AppLayout from './layout/AppLayout';
+import { CssBaseline } from '@mui/material';
 
 export default function App() {
   return (
     <React.Fragment>
+      <CssBaseline />
       <AppLayout />
     </React.Fragment>
   );
--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,50 +1,162 @@
 import * as React from 'react';
-import { Fragment } from 'react';
-import Appbar from './Appbar';
-import { CssBaseline, Grid } from '@mui/material';
-import SideMenu from './SideMenu';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
 import NewCustomer from '../customer/NewCustomer';
-import ReceiptFrame from './ReceiptFrame';
 import Box from '@mui/material/Box';
+import { CSSObject, styled, Theme, useTheme } from '@mui/material/styles';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Typography from '@mui/material/Typography';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import ListItemText from '@mui/material/ListItemText';
+import MuiDrawer from '@mui/material/Drawer';
 
-function AppLayout() {
-  return (
-    <Fragment>
-      <CssBaseline />
-      <Box sx={{ height: '100%', display: 'flex' }} flexDirection="column">
-        <Appbar />
-        <Box id="cont" sx={{ display: 'flex', alignItems: 'stretch', height: '100%' }}>
-          <Box sx={{ backgroundColor: 'primary.main', padding: '1em 0 0 0' }}><SideMenu /></Box>
-          <Box sx={{ overflowY: 'auto', margin: '1em 2em 0 2em' }} flexGrow={1}><NewCustomer /></Box>
-          <Box bgcolor="lightpink" flex="1">receipt</Box>
-        </Box>
-      </Box>
-    </Fragment>
-  );
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
 }
 
-function old() {
+const drawerWidth = 240;
+
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    ...(open && {
+      ...openedMixin(theme),
+      '& .MuiDrawer-paper': openedMixin(theme),
+    }),
+    ...(!open && {
+      ...closedMixin(theme),
+      '& .MuiDrawer-paper': closedMixin(theme),
+    }),
+  }),
+);
+
+function AppLayout() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
   return (
-    <Fragment>
-      <CssBaseline />
-      <Appbar />
-      <Grid container justifyContent="stretch" justifyItems="stretch" justifySelf="stretch" direction="row">
-        <Grid item xs={1} container direction="column" bgcolor="azure">
-          <SideMenu />
-        </Grid>
-        <Grid container item direction="row" justifyContent="space-evenly" xs rowSpacing={2} columnSpacing={4}>
-          {/*<Box mr={10} ml={10}>*/}
-          <NewCustomer />
-          {/*</Box>*/}
-
-        </Grid>
-        <Grid xs={3}>
-          <ReceiptFrame />
-        </Grid>
-      </Grid>
-    </Fragment>
+    <Box sx={{ display: 'fixed', height: '100%' }}>
+      <AppBar position="fixed" elevation={0} sx={{ height: '4em' }} open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{
+              marginRight: 5,
+              ...(open && { display: 'none' }),
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">Pawnflow </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" open={open}>
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItemButton
+              key={text}
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? 'initial' : 'center',
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : 'auto',
+                  justifyContent: 'center',
+                }}
+              >
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          ))}
+        </List>
+        <Divider />
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, alignItems: 'stretch', height: '100%', display: 'flex', pt: '4em' }}>
+        <Box sx={{ overflowY: 'auto', margin: '1em 2em 0 2em' }} flexGrow={1}><NewCustomer /></Box>
+        <Box bgcolor="lightpink" flex="1">receipt</Box>
+      </Box>
+    </Box>
   );
-
 }
 
 export default AppLayout;
